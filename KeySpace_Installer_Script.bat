3033b547504e6d68c61d968b27fc57d63a56917f
Velocity07 <87845181+Velocity07@users.noreply.github.com>
2024-07-01 17:36:51 +0530

    Add Files

---
+++ KeySpace_Installer_Script.bat
@@ -0,0 +1,29 @@
+@echo off
+title Connecting to Blunt Mark Server's...
+color 81
+set load=
+set/a loadnum=0
+:Loading
+set load=%load%лл
+cls
+echo.
+echo Connecting to Server...
+echo -----------------------------------------
+echo %load%                    
+echo -----------------------------------------
+echo.
+echo.
+echo Made by Velocity7
+ping localhost -n 2 >nul
+set/a loadnum=%loadnum% +1
+if %loadnum%==20 goto Done
+goto Loading
+:Done
+echo.
+pause
+title {Connection Established}
+cls
+tree
+pip install customtkinter
+move "%UserProfile%\Downloads\KeySpace Package\KeySpace" "%AppData%\KeySpace"
+move "%AppData%\KeySpace\ShortCut\*.*" "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
