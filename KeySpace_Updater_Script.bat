7c1e6fed26bae9826d3d641d772e0a729d69a086
Velocity07 <87845181+Velocity07@users.noreply.github.com>
2024-08-27 22:12:21 +0530

    Add KeySpace_Updater_Script.bat

---
+++ KeySpace_Updater_Script.bat
@@ -0,0 +1,29 @@
+@echo off
+title Updating KeySpace...
+color 81
+set load=
+set/a loadnum=0
+:Loading
+set load=%load%лл
+cls
+echo.
+echo Loading Data...
+echo -----------------------------------------
+echo %load%                    
+echo -----------------------------------------
+echo.
+echo.
+echo Made by Velocity7
+ping localhost -n 2 >nul
+set/a loadnum=%loadnum% +1
+if %loadnum%==20 goto Done
+goto Loading
+:Done
+echo.
+pause
+cls
+tree
+pip install customtkinter
+rename "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\KeySpace V1.5.lnk" "KeySpace V1.8.lnk"
+rmdir /q /s %AppData%\KeySpace
+move "%UserProfile%\Downloads\KeySpace_Package_V1.8\KeySpace" "%AppData%\KeySpace"
